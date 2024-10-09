c1dd97a12df2433c136084e152467895b06fab1a
Олег Дербин <Olegmanchik@gmail.com>
2024-10-09 16:12:54 +0300

    create new project

---
+++ Exam21Lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam21Lesson
+//
+//  Created by Олег Дербин on 09.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
